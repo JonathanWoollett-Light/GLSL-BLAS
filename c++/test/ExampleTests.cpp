b0067c5890e78d4d7bbe5d3fcf3188ecd5559684
Jonathan Woollett-Light <jonathanwoollettlight@gmai.com>
2021-02-13 22:53:15 +0000

    SAXPY tests

--- c++/test/ExampleTests.cpp
+++ c++/test/ExampleTests.cpp
@@ -107,90 +107,99 @@ TEST(SSCAL, random) {
     }
 }
 
-// TEST(SAXPY, one) {
-//     uint32_t size = 10;
-//     float** data = new float*[2];
-//     data[0] = new float[size]{ 0,1,2,3,4,5,6,7,8,9 };
-//     data[1] = new float[size]{ 9,8,7,6,5,4,3,2,1,0 };
+TEST(SAXPY, one) {
+    uint32_t const numBuffers = 2;
+    uint32_t const numPushConstants = 1;
+    uint32_t const size = 10;
 
-//     char const shader[] = "../../../glsl/saxpy.spv";
-//     ComputeApp app = ComputeApp(
-//         shader,
-//         2, //  Number of buffers
-//         new uint32_t[2]{ size, size }, // Buffer sizes
-//         data, // Buffer data
-//         1, // Number of push constants
-//         new float[1]{ 1 }, // Push constants
-//         new uint32_t[3]{ size,1,1 }, // Invocations
-//         new uint32_t[3]{ WORKGROUP_SIZE,1,1 } // Workgroup sizes
-//     );
-//     float* out = static_cast<float*>(Utility::map(app.device,app.bufferMemory[1]));
-//     for(uint32_t i = 0; i < size; ++i) {
-//         //std::cout << out[i] << std::endl;
-//         ASSERT_EQ(i+(size-i-1),out[i]);
-//     }
-// }
+    float** data = new float*[numBuffers];
+    data[0] = new float[size]{ 0,1,2,3,4,5,6,7,8,9 };
+    data[1] = new float[size]{ 9,8,7,6,5,4,3,2,1,0 };
 
-// TEST(SAXPY, two) {
-//     uint32_t size = 10;
-//     float** data = new float*[2];
-//     data[0] = new float[size]{ 0,1,2,3,4,5,6,7,8,9 };
-//     data[1] = new float[size]{ 9,8,7,6,5,4,3,2,1,0 };
+    char const shader[] = "../../../glsl/saxpy.spv";
+    ComputeApp<numPushConstants> app = ComputeApp<numPushConstants>(
+        shader,
+        2, //  Number of buffers
+        new uint32_t[numBuffers]{ size, size }, // Buffer sizes
+        data, // Buffer data
+        std::array<std::variant<uint32_t,float>, numPushConstants> { 1.0F }, // Push constants
+        new uint32_t[3]{ size,1,1 }, // Invocations
+        new uint32_t[3]{ WORKGROUP_SIZE,1,1 } // Workgroup sizes
+    );
 
-//     char const shader[] = "../../../glsl/saxpy.spv";
-//     ComputeApp app = ComputeApp(
-//         shader,
-//         2, //  Number of buffers
-//         new uint32_t[2]{ size, size }, // Buffer sizes
-//         data, // Buffer data
-//         1, // Number of push constants
-//         new float[1]{ 2 }, // Push constants
-//         new uint32_t[3]{ size,1,1 }, // Invocations
-//         new uint32_t[3]{ WORKGROUP_SIZE,1,1 } // Workgroup sizes
-//     );
-//     float* out = static_cast<float*>(Utility::map(app.device,app.bufferMemory[1]));
-//     for(uint32_t i = 0; i < size; ++i) {
-//         //std::cout << out[i] << std::endl;
-//         ASSERT_EQ(2*i+(size-i-1),out[i]);
-//     }
-// }
+    float* out = static_cast<float*>(Utility::map(app.device,app.bufferMemory[1]));
+    for(uint32_t i = 0; i < size; ++i) {
+        //std::cout << out[i] << std::endl;
+        ASSERT_EQ(i+(size-i-1),out[i]);
+    }
+}
 
-// TEST(SAXPY, random) {
-//     srand((unsigned int)time(NULL));
+TEST(SAXPY, two) {
+    uint32_t const numBuffers = 2;
+    uint32_t const numPushConstants = 1;
+    uint32_t const size = 10;
 
-//     for(uint32_t i=0;i<RAND_RUNS;++i) {
+    float** data = new float*[numBuffers];
+    data[0] = new float[size]{ 0,1,2,3,4,5,6,7,8,9 };
+    data[1] = new float[size]{ 9,8,7,6,5,4,3,2,1,0 };
 
-//         uint32_t size = MIN_SIZE + (rand() % uint32_t(MAX_SIZE - MIN_SIZE + 1));
+    char const shader[] = "../../../glsl/saxpy.spv";
+    ComputeApp<numPushConstants> app = ComputeApp<numPushConstants>(
+        shader,
+        2, //  Number of buffers
+        new uint32_t[numBuffers]{ size, size }, // Buffer sizes
+        data, // Buffer data
+        std::array<std::variant<uint32_t,float>, numPushConstants> { 2.0F }, // Push constants
+        new uint32_t[3]{ size,1,1 }, // Invocations
+        new uint32_t[3]{ WORKGROUP_SIZE,1,1 } // Workgroup sizes
+    );
 
-//         float** data = new float*[2];
-//         data[0] = new float[size];
-//         data[1] = new float[size];
+    float* out = static_cast<float*>(Utility::map(app.device,app.bufferMemory[1]));
+    for(uint32_t i = 0; i < size; ++i) {
+        //std::cout << out[i] << std::endl;
+        ASSERT_EQ(2*i+(size-i-1),out[i]);
+    }
+}
 
-//         for(uint32_t j=0;j<size;++j) {
-//             data[0][j] = float(rand())/float(RAND_MAX);
-//             data[1][j] = float(rand())/float(RAND_MAX);
-//         }
+TEST(SAXPY, random) {
+    srand((unsigned int)time(NULL));
 
-//         float push_constant = float(rand())/float(RAND_MAX);
+    uint32_t const numBuffers = 2;
+    uint32_t const numPushConstants = 1;
 
-//         char const shader[] = "../../../glsl/saxpy.spv";
-//         ComputeApp app = ComputeApp(
-//             shader,
-//             2, //  Number of buffers
-//             new uint32_t[2]{ size, size }, // Buffer sizes
-//             data, // Buffer data
-//             1, // Number of push constants
-//             new float[1]{ push_constant }, // Push constants
-//             new uint32_t[3]{ size,1,1 }, // Invocations
-//             new uint32_t[3]{ WORKGROUP_SIZE,1,1 } // Workgroup sizes
-//         );
-//         float* out = static_cast<float*>(Utility::map(app.device,app.bufferMemory[1]));
-//         for(uint32_t i = 0; i < size; ++i) {
-//             //std::cout << out[i] << std::endl;
-//             ASSERT_EQ(push_constant*data[0][i]+data[1][i],out[i]);
-//         }
-//     }
-// }
+    for(uint32_t i=0;i<RAND_RUNS;++i) {
+
+        uint32_t const size = MIN_SIZE + (rand() % uint32_t(MAX_SIZE - MIN_SIZE + 1));
+
+        float** data = new float*[numBuffers];
+        data[0] = new float[size];
+        data[1] = new float[size];
+
+        for(uint32_t j=0;j<size;++j) {
+            data[0][j] = float(rand())/float(RAND_MAX);
+            data[1][j] = float(rand())/float(RAND_MAX);
+        }
+
+        float const pushConstant = float(rand())/float(RAND_MAX);
+
+        char const shader[] = "../../../glsl/saxpy.spv";
+        ComputeApp<numPushConstants> app = ComputeApp<numPushConstants>(
+            shader,
+            2, //  Number of buffers
+            new uint32_t[numBuffers]{ size, size }, // Buffer sizes
+            data, // Buffer data
+            std::array<std::variant<uint32_t,float>, numPushConstants> { pushConstant }, // Push constants
+            new uint32_t[3]{ size,1,1 }, // Invocations
+            new uint32_t[3]{ WORKGROUP_SIZE,1,1 } // Workgroup sizes
+        );
+
+        float* out = static_cast<float*>(Utility::map(app.device,app.bufferMemory[1]));
+        for(uint32_t i = 0; i < size; ++i) {
+            //std::cout << out[i] << std::endl;
+            ASSERT_EQ(pushConstant*data[0][i]+data[1][i],out[i]);
+        }
+    }
+}
 
 // 1 subgroup worth (10)
 TEST(SDOT_F, one) {

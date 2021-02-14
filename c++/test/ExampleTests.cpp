d761f5b8996d6229b408c96923b810e023787479
Jonathan Woollett-Light <jonathanwoollettlight@gmai.com>
2021-02-14 18:11:52 +0000

    SNRM2 tests

--- c++/test/ExampleTests.cpp
+++ c++/test/ExampleTests.cpp
@@ -18,6 +18,9 @@ const uint32_t MIN_SIZE = 100000;
 //  Also maybe use percentage difference instead.
 const float EPSILON = 0.1;
 
+// sscal
+// -----------------------------------------
+
 TEST(SSCAL, one) {
     uint32_t const numBuffers = 1;
     uint32_t const numPushConstants = 1;
@@ -106,6 +109,9 @@ TEST(SSCAL, random) {
         }
     }
 }
+
+// saxpy
+// -----------------------------------------
 
 TEST(SAXPY, one) {
     uint32_t const numBuffers = 2;
@@ -200,6 +206,9 @@ TEST(SAXPY, random) {
         }
     }
 }
+
+// sdot_f
+// -----------------------------------------
 
 // 1 subgroup worth (10)
 TEST(SDOT_F, one) {
@@ -382,6 +391,162 @@ TEST(SDOT_F, random) {
         for(uint32_t i = 0; i < size; ++i) {
             //std::cout << out[i] << std::endl;
             sum += data[0][i] * data[1][i];
+        }
+        std::cout << *out << std::endl;
+        ASSERT_NEAR(*out,sum,EPSILON);
+    }
+}
+
+// snrm2_f
+// -----------------------------------------
+
+// 1 subgroup worth (10)
+TEST(SNRM2_F, one) {
+    uint32_t const numBuffers = 2;
+    uint32_t const numPushConstants = 1;
+    uint32_t const size = 10;
+
+    float** data = new float*[numBuffers];
+    data[0] = new float[size]{ 0,1,2,3,4,5,6,7,8,9 };
+    data[1] = new float[1];
+
+    char const shader[] = "../../../glsl/snrm2_f.spv";
+
+    ComputeApp<numPushConstants> app = ComputeApp<numPushConstants>(
+        shader,
+        numBuffers, //  Number of buffers
+        new uint32_t[numBuffers]{ size, 1 }, // Buffer sizes
+        data, // Buffer data
+        std::array<std::variant<uint32_t,float>, numPushConstants> { size }, // Push constants
+        new uint32_t[3]{ 1,1,1 }, // Invocations
+        new uint32_t[3]{ WORKGROUP_SIZE,1,1 } // Workgroup sizes
+    );
+    float* out = static_cast<float*>(Utility::map(app.device,app.bufferMemory[1]));
+    std::cout << *out << std::endl;
+    ASSERT_NEAR(*out,285.0,EPSILON);
+    //assert(false);
+}
+
+// 2 subgroups worth (70)
+TEST(SNRM2_F, two) {
+    uint32_t const numBuffers = 2;
+    uint32_t const numPushConstants = 1;
+    uint32_t const size = 70;
+
+    float** data = new float*[numBuffers];
+    data[0] = new float[size]{ 
+        0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9
+    };
+    data[1] = new float[1];
+
+    char const shader[] = "../../../glsl/snrm2_f.spv";
+
+    ComputeApp<numPushConstants> app = ComputeApp<numPushConstants>(
+        shader,
+        numBuffers, //  Number of buffers
+        new uint32_t[numBuffers]{ size, 1 }, // Buffer sizes
+        data, // Buffer data
+        std::array<std::variant<uint32_t,float>, numPushConstants> { size }, // Push constants
+        new uint32_t[3]{ 1,1,1 }, // Invocations
+        new uint32_t[3]{ WORKGROUP_SIZE,1,1 } // Workgroup sizes
+    );
+    float* out = static_cast<float*>(Utility::map(app.device,app.bufferMemory[1]));
+    std::cout << *out << std::endl;
+    ASSERT_NEAR(*out,1995.0,EPSILON); // 285*7
+    //assert(false);
+}
+
+// 2 workgroups worth (1050)
+TEST(SNRM2_F, three) {
+    uint32_t const numBuffers = 2;
+    uint32_t const numPushConstants = 1;
+    uint32_t const size = 1050;
+
+    float** data = new float*[numBuffers];
+    // 21 x 50
+    data[0] = new float[size]{ 
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,
+        0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9
+    };
+    data[1] = new float[1];
+
+    char const shader[] = "../../../glsl/snrm2_f.spv";
+
+    ComputeApp<numPushConstants> app = ComputeApp<numPushConstants>(
+        shader,
+        numBuffers, //  Number of buffers
+        new uint32_t[numBuffers]{ size, 1 }, // Buffer sizes
+        data, // Buffer data
+        std::array<std::variant<uint32_t,float>, numPushConstants> { size }, // Push constants
+        new uint32_t[3]{ 1,1,1 }, // Invocations
+        new uint32_t[3]{ WORKGROUP_SIZE,1,1 } // Workgroup sizes
+    );
+    float* out = static_cast<float*>(Utility::map(app.device,app.bufferMemory[1]));
+    std::cout << *out << std::endl;
+    ASSERT_NEAR(*out,29925,EPSILON); // 285 * 5 * 21
+    //assert(false);
+}
+
+TEST(SNRM2_F, random) {
+    srand((unsigned int)time(NULL));
+
+    uint32_t const numBuffers = 2;
+    uint32_t const numPushConstants = 1;
+
+    for(uint32_t i=0;i<RAND_RUNS;++i) {
+        uint32_t max = WORKGROUP_SIZE*WORKGROUP_SIZE;
+        uint32_t min = WORKGROUP_SIZE+1;
+        uint32_t size = min + (rand() % uint32_t(max - min + 1));
+
+        float** data = new float*[numBuffers];
+        data[0] = new float[size];
+        data[1] = new float[1];
+
+        for(uint32_t j=0;j<size;++j) {
+            data[0][j] = float(rand())/float(RAND_MAX);
+        }
+
+        char const shader[] = "../../../glsl/snrm2_f.spv";
+
+        ComputeApp<numPushConstants> app = ComputeApp<numPushConstants>(
+            shader,
+            numBuffers, //  Number of buffers
+            new uint32_t[numBuffers]{ size, 1 }, // Buffer sizes
+            data, // Buffer data
+            std::array<std::variant<uint32_t,float>, numPushConstants> { size }, // Push constants
+            new uint32_t[3]{ 1,1,1 }, // Invocations
+            new uint32_t[3]{ WORKGROUP_SIZE,1,1 } // Workgroup sizes
+        );
+        float* out = static_cast<float*>(Utility::map(app.device,app.bufferMemory[1]));
+        float sum = 0;
+        for(uint32_t i = 0; i < size; ++i) {
+            //std::cout << out[i] << std::endl;
+            sum += data[0][i] * data[0][i];
         }
         std::cout << *out << std::endl;
         ASSERT_NEAR(*out,sum,EPSILON);

47f76d167e1ef0ea4adcdf4df572d9d682b5c467
Jonathan <jonthanwoollettlight@gmail.com>
2021-04-13 15:13:24 +0100

    Double cpu benchmarks

--- rust/benches/benches.rs
+++ rust/benches/benches.rs
@@ -99,4 +99,93 @@ mod benches {
 
         bencher.iter(|| a.dot(&b).mapv(|v|v*alpha)+c.mapv(|v|v*beta));
     }
+
+    #[bench]
+    fn dscal(bencher: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+        let vals: Vec<f64> = (0..SIZE).map(|_|rng.gen()).collect();
+        let x = arr1(&vals);
+
+        bencher.iter(|| x.mapv(|v|v*2.));
+    }
+    #[bench]
+    fn daxpy(bencher: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+        let x_vals:Vec<f64> = (0..SIZE).map(|_|rng.gen()).collect();
+        let y_vals:Vec<f64> = (0..SIZE).map(|_|rng.gen()).collect();
+
+        let x = arr1(&x_vals);
+        let y = arr1(&y_vals);
+
+        bencher.iter(|| x.mapv(|v|v*2.)+&y);
+    }
+    #[bench]
+    fn ddot(bencher: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+        let x_vals:Vec<f64> = (0..SIZE).map(|_|rng.gen()).collect();
+        let y_vals:Vec<f64> = (0..SIZE).map(|_|rng.gen()).collect();
+
+        let x = arr1(&x_vals);
+        let y = arr1(&y_vals);
+
+        bencher.iter(|| (&x*&y).sum());
+    }
+    #[bench]
+    fn dnrm2(bencher: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+        let x_vals:Vec<f64> = (0..SIZE).map(|_|rng.gen()).collect();
+        let x = arr1(&x_vals);
+
+        bencher.iter(|| x.mapv(|v|v*v).sum());
+    }
+    #[bench]
+    fn dasum(bencher: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+        let x_vals:Vec<f64> = (0..SIZE).map(|_|rng.gen()).collect();
+        let x = arr1(&x_vals);
+
+        bencher.iter(|| x.mapv(|v|v.abs()).sum());
+    }
+    #[bench]
+    fn idamax(bencher: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+        let x_vals:Vec<f64> = (0..SIZE).map(|_|rng.gen()).collect();
+        let x = arr1(&x_vals);
+
+        bencher.iter(|| x.iter().max_by(|a,bencher|a.partial_cmp(bencher).unwrap()));
+    }
+    #[bench]
+    fn dgemv(bencher: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+        let sqrt_size = (SIZE as f32).sqrt() as usize;
+        let a_vals:Vec<f64> = (0..sqrt_size * sqrt_size).map(|_|rng.gen()).collect();
+        let x_vals:Vec<f64> = (0..sqrt_size).map(|_|rng.gen()).collect();
+        let y_vals:Vec<f64> = (0..sqrt_size).map(|_|rng.gen()).collect();
+
+        let a = Array::from_shape_vec((sqrt_size,sqrt_size),a_vals).unwrap();
+        let x = arr1(&x_vals);
+        let y = arr1(&y_vals);
+
+        let alpha = 2f64;
+        let beta = 3f64;
+
+        bencher.iter(|| a.dot(&x).mapv(|v|v*alpha)+y.mapv(|v|v*beta));
+    }
+    #[bench]
+    fn dgemm(bencher: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+        let sqrt_size = (SIZE as f32).sqrt() as usize;
+        let a_vals:Vec<f64> = (0..sqrt_size*sqrt_size).map(|_|rng.gen()).collect();
+        let b_vals:Vec<f64> = (0..sqrt_size*sqrt_size).map(|_|rng.gen()).collect();
+        let c_vals:Vec<f64> = (0..sqrt_size*sqrt_size).map(|_|rng.gen()).collect();
+
+        let a = Array::from_shape_vec((sqrt_size,sqrt_size),a_vals).unwrap();
+        let b = Array::from_shape_vec((sqrt_size,sqrt_size),b_vals).unwrap();
+        let c = Array::from_shape_vec((sqrt_size,sqrt_size),c_vals).unwrap();
+
+        let alpha = 2f64;
+        let beta = 3f64;
+
+        bencher.iter(|| a.dot(&b).mapv(|v|v*alpha)+c.mapv(|v|v*beta));
+    }
 }
